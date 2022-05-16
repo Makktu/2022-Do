56068a3d1ee9ecd583b968966050c0a4aaea786b
Makktu <johnmcna@gmail.com>
2022-05-16 18:45:25 +0100

    working on new entries

--- data.js
+++ data.js
@@ -24,3 +24,67 @@ export let months = [
     "December",
     12,
 ];
+
+export let examplesTodo = [
+    {
+        entry: 1,
+        date: "12 May 2022",
+        content: "Wash Car",
+    },
+    {
+        entry: 2,
+        date: "15 May 2022",
+        content: "Pay bills",
+    },
+    {
+        entry: 3,
+        date: "31 May 2019",
+        content: "Check council website for that thing",
+    },
+    {
+        entry: 4,
+        date: "2 June 2022",
+        content: "Attend the match",
+    },
+    {
+        entry: 5,
+        date: "19 May 2022",
+        content: "get Steve's birthday present",
+    },
+    {
+        entry: 6,
+        date: "2 June 1988",
+        content: "Attend the match",
+    },
+    {
+        entry: 7,
+        date: "4 February 2023",
+        content: "get Steve's birthday present",
+    },
+
+    {
+        entry: 8,
+        date: "22 November 2022",
+        content: "Attend the match",
+    },
+    {
+        entry: 9,
+        date: "14 April 2025",
+        content: "close bank accounts",
+    },
+    {
+        entry: 10,
+        date: "15 January 2023",
+        content: "Kira's wedding",
+    },
+    {
+        entry: 11,
+        date: "10 March 2022",
+        content: "paint shed",
+    },
+    {
+        entry: 12,
+        date: "1 June 2022",
+        content: "paint house",
+    },
+];
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-import { months } from "./data.js";
+import { months, examplesTodo } from "./data.js";
+
+function createNew() {
+    console.log("yep");
+}
 
 function displayList(todoList) {
     for (let entry of todoList) {
@@ -13,13 +17,11 @@ function displayList(todoList) {
 }
 
 function sortDates() {
-    // make a copy of the ToDo list and work on it for safety's sake
     let tempToDos = todoList;
 
     let theYear;
     let theMonth;
     let theDay;
-    // first sort by year, ascending order - get last 4 characters of .date, convert to number, and sort. Shuffle the tempArray back into
 
     for (let entry of todoList) {
         theYear = +entry.date.substring(entry.date.length - 4);
@@ -30,10 +32,8 @@ function sortDates() {
         return a.year - b.year;
     });
 
-    // now do same for the month
     for (let entry of tempToDos) {
         theMonth = entry.date.split(" ")[1];
-        console.log(theMonth[1]);
         let count = 0;
         for (let month of months) {
             if (theMonth == month) {
@@ -43,7 +43,7 @@ function sortDates() {
             count++;
         }
     }
-    // now the same for the days
+
     for (let entry of tempToDos) {
         theDay = +entry.date.split(" ")[0];
         entry.day = theDay;
@@ -69,70 +69,10 @@ function sortDates() {
 }
 
 const displayTodos = document.querySelector(".todo-display");
+const createBtn = document.querySelector(".create-new");
+createBtn.addEventListener("click", createNew);
 
-let todoList = [
-    {
-        entry: 1,
-        date: "12 May 2022",
-        content: "Wash Car",
-    },
-    {
-        entry: 2,
-        date: "15 May 2022",
-        content: "Pay bills",
-    },
-    {
-        entry: 3,
-        date: "31 May 2019",
-        content: "Check council website for that thing",
-    },
-    {
-        entry: 4,
-        date: "2 June 2022",
-        content: "Attend the match",
-    },
-    {
-        entry: 5,
-        date: "19 May 2022",
-        content: "get Steve's birthday present",
-    },
-    {
-        entry: 6,
-        date: "2 June 1988",
-        content: "Attend the match",
-    },
-    {
-        entry: 7,
-        date: "4 February 2023",
-        content: "get Steve's birthday present",
-    },
-
-    {
-        entry: 8,
-        date: "22 November 2022",
-        content: "Attend the match",
-    },
-    {
-        entry: 9,
-        date: "14 April 2025",
-        content: "close bank accounts",
-    },
-    {
-        entry: 10,
-        date: "15 January 2023",
-        content: "Kira's wedding",
-    },
-    {
-        entry: 11,
-        date: "10 March 2022",
-        content: "paint shed",
-    },
-    {
-        entry: 12,
-        date: "1 June 2022",
-        content: "paint house",
-    },
-];
+let todoList = examplesTodo;
 
 sortDates();
 displayList(todoList);
@@ -143,4 +83,37 @@ editBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         console.log(e.target.id);
     });
+});
+
+const modal = document.querySelector("#modal");
+
+const dateToDo = document.getElementById("dateToDo");
+const saveBtn = document.querySelector(".btn-save");
+const theToDo = document.querySelector(".the-todo");
+saveBtn.addEventListener("click", () => {
+    console.log(theToDo.value);
+    if (theToDo.value) {
+        let toDoText = theToDo.value;
+        console.log(toDoText);
+        modal.closeModal();
+        // add date and content to the object array
+        // and call refreshed display
+    }
+});
+
+// const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+createBtn.addEventListener("click", () => {
+    let todaysDate =
+        `${new Date().getDate()} ` +
+        `${new Date().toLocaleString("default", { month: "long" })} ` +
+        `${new Date().getFullYear()}` +
+        "";
+    modal.showModal();
+    dateToDo.textContent = todaysDate;
+});
+
+closeModal.addEventListener("click", () => {
+    modal.close();
 });
